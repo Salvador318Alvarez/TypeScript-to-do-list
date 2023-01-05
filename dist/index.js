d59993a4ed80e61835494b8f1c9d2260210567a9
Salvador318Alvarez <Salvador318Alvarez@gmail.com>
2023-01-04 18:43:45 -0600

    changes

--- dist/index.js
+++ dist/index.js
@@ -2,16 +2,16 @@ import {v4 as uuidv4 } from 'uuid'
 
 // <type>
 
-const list = document.querySelector<HTMLUListElement>("#list")
+const list = document.querySelector("#list")
 const form = document.getElementById("new-task-form")
-const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const input = document.querySelector("#new-task-title")
 const tasks = loadTasks()
 tasks.forEach(addListItem)
 
-form?.addEventListener("submit", e => {
+form.addEventListener("submit", e => {
   e.preventDefault()
 
-  if (input?.value == "" || input?.value == null) return
+  if (input.value == "" || input?.value == null) return
 
   const newTask = {
     id: uuidv4(),
@@ -38,7 +38,7 @@ function addListItem(task) {
   checkbox.checked = task.completed
   label.append(checkbox, task.title)
   item.append(label)
-  list?.append(item)  
+  list.append(item)  
   
 }
 
